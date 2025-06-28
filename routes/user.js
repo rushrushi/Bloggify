1ed17ca3a5909ba5127de302531f7b38b0ec6f7d
Rushi Dingankar <126055256+rushrushi@users.noreply.github.com>
2025-06-28 11:59:29 +0530

    Add files via upload

---
+++ routes/user.js
@@ -0,0 +1,48 @@
+const {Router} = require('express')
+const User = require('../models/user')
+const { createTokenForUser } = require('../services/authentication');
+const router = Router()
+
+router.get('/signin', (req,res) => {
+    return res.render('signin')
+})
+
+
+router.get('/signup', (req, res) => {
+    return res.render('signup')
+})
+
+
+router.post('/signin', async (req, res) => {
+    const {email, password} = req.body
+    try{
+        const token = await User.matchPasswordAndGenerateToken(email, password)
+
+        res.cookie("token", token).redirect('/')
+    } catch (error){
+        res.render('signin', {
+            error: "Incorrect Email or Password"
+        })
+    }
+    
+})
+
+router.get('/logout', (req, res) => {
+    return res.clearCookie("token").redirect('/')
+})
+
+router.post('/signup', async (req, res) => {
+    const { fullName, email, password, profileImageURl } = req.body
+    await User.create(
+        {fullName,
+        email,
+        password
+    })
+    const token = createTokenForUser(User)
+    console.log("User created", token);
+    return res.cookie("token", token).redirect('/')
+})
+
+
+
+module.exports = router
